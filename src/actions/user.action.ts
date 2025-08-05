a784ef984e5ecb485c00b932a1aa79e7baa27158
Vedrakholia <vedrakholia32@gmail.com>
2025-08-05 15:23:06 -0400

    Like, Comment and Delete Post

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -55,7 +55,7 @@ export async function getUserByClerkId(clerkId: string) {
 export async function getDbUserId() {
   const { userId: clerkId } = await auth();
 
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
 
@@ -67,6 +67,7 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
   try {
     const userId = await getDbUserId();
+    if (!userId) return [];
 
     const randomUsers = await prisma.user.findMany({
       where: {
@@ -107,6 +108,7 @@ export async function getRandomUsers() {
 export async function toggleFollow(targetUserId: string) {
   try {
     const userId = await getDbUserId();
+    if (!userId) return;
 
     if (userId === targetUserId) throw new Error("You cannot follow yourself");
 
@@ -147,7 +149,7 @@ export async function toggleFollow(targetUserId: string) {
       ]);
     }
 
-    revalidatePath("/")
+    revalidatePath("/");
     return { success: true };
   } catch (error) {
     console.log("Error in toggleFollow", error);
