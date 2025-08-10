2dce57e3a5f80ff5a13c5643374802b5f78efe26
Vedrakholia <vedrakholia32@gmail.com>
2025-08-09 20:47:50 -0400

    fixed mobile view bug

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -12,13 +12,14 @@ import {
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { useState } from "react";
-import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { useAuth, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 
 function MobileNavbar() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
     const { isSignedIn } = useAuth();
+    const { user } = useUser();
     const { theme, setTheme } = useTheme();
 
     return (
@@ -61,7 +62,7 @@ function MobileNavbar() {
                                     </Link>
                                 </Button>
                                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
-                                    <Link href="/profile">
+                                    <Link href={`/profile/${user?.username ?? user?.emailAddresses?.[0]?.emailAddress?.split("@")[0]}`}>
                                         <UserIcon className="w-4 h-4" />
                                         Profile
                                     </Link>
